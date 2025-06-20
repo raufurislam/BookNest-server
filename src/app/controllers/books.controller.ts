868f728c675f585d872216d91f495afd8fa826b6
Raufur Islam <raufurislam@gmail.com>
2025-06-21 02:29:58 +0600

    Basic all book route

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -46,3 +46,25 @@ booksRoutes.get("/", async (req: Request, res: Response) => {
     data: books,
   });
 });
+
+booksRoutes.get("/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+  const book = await Book.findById(bookId);
+  res.status(201).json({
+    success: true,
+    message: "Book retrieved successfully",
+    data: book,
+  });
+});
+
+booksRoutes.patch("/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+  const updatedBody = req.body;
+  const book = await Book.findByIdAndUpdate(bookId, updatedBody, { new: true });
+
+  res.status(200).json({
+    success: true,
+    message: "Book updated successfully",
+    data: book,
+  });
+});
